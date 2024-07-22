25f9613c672ee7373f7ecfbdeca3d2f3022dd4ae
AdventurousProgrammer <gitrepowizard@gmail.com>
2024-07-22 17:22:32 -0500

    updated to do list code to include arrow functions

--- Javascript/Lesson 12 - Advanced Functions/12-todo-list.js
+++ Javascript/Lesson 12 - Advanced Functions/12-todo-list.js
@@ -1,38 +1,42 @@
+const addButtonElement = document.querySelector('.js-add-to-do-button');
+addButtonElement.addEventListener('click', () => {
+    addToDo();
+});
+
+
 const toDoList = [];
 // {name: 'Finish Coding', date: '07/11/2024'}
 function removeItem(i)
 {
     toDoList.splice(i, 1);
 }
+
 function renderToDoList()
 {
     let htmlString = '';
     toDoList.forEach(function(toDoObject, index) {
         const {name, date} = toDoObject
+
+        // this is just a string, element needs to be on document
         const html = `<div class="js-item-name">${name}</div>
-                        <div class="js-item-date">${date} </div>
+                        <div class="js-item-date">${date} </div>  
                         <div>
-                            <button class="delete-button" onclick="toDoList.splice(${index}, 1);
+                            <button class="delete-button js-delete-to-do-button" onclick="toDoList.splice(${index}, 1);
                                                                    renderToDoList();">Delete</button>
                         </div>
                         `;
          htmlString += html;
     });
-    /*
-    for(let i = 0; i < toDoList.length; i++)
-    {
-        const {name, date} = toDoObject
-         const html = `<div class="js-item-name">${name}</div>
-                        <div class="js-item-date">${date} </div>
-                        <div>
-                            <button class="delete-button" onclick="toDoList.splice(${i}, 1);">Delete</button>
-                        </div>
-                        `;
-         htmlString += html;
-    }
-         */
+    
     document.querySelector('.js-to-do-list')
         .innerHTML = htmlString;
+
+    // element needs to be on page in order to be selected
+   document.querySelectorAll('.js-delete-to-do-button').forEach((deleteButton, index) => {
+    deleteButton.addEventListener('click', () => {
+            toDoList.splice(index, 1);
+        });
+   });
 }
 
 function addToDo()
@@ -47,6 +51,7 @@ function addToDo()
         name: toDoItemName,
         date: toDoItemDate
       };
+
     toDoList.push(toDoItem);
     renderToDoList();
 }
